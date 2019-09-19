13b71e1450f02699ce43db129aec37764e412297
zakiarsyad <ozikarsyad@gmail.com>
2019-09-19 20:36:55 +0700

    join room

--- client/src/router.js
+++ client/src/router.js
@@ -22,7 +22,7 @@ export default new Router({
       component: () => import(/* webpackChunkName: "about" */ './views/Room.vue'),
     },
     {
-      path: '/game',
+      path: '/game/:id',
       name: 'game',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
@@ -30,7 +30,7 @@ export default new Router({
       component: () => import(/* webpackChunkName: "about" */ './views/Game.vue'),
     },
     {
-      path: '/leaderboard',
+      path: '/leaderboard/:id',
       name: 'leaderboard',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
