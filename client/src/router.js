9cee758740b8803533abf696c0e41ea47922ff70
jackbloo <a.iqbal1404@gmail.com>
2019-09-19 17:07:50 +0700

    first time

---
+++ client/src/router.js
@@ -0,0 +1,41 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
+
+Vue.use(Router);
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/home',
+      name: 'home',
+      component: Home,
+    },
+    {
+      path: '/room',
+      name: 'room',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/Room.vue'),
+    },
+    {
+      path: '/game',
+      name: 'game',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/Game.vue'),
+    },
+    {
+      path: '/leaderboard',
+      name: 'leaderboard',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/Leaderboard.vue'),
+    },
+  ],
+});
